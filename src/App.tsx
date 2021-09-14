2b4232e1913fd949ce6061c8f3401a893a8358c1
layla <minytiny@163.com>
2021-09-14 17:08:29 +0800

    fix: path alias does not take effect

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React, { memo } from "react";
 import {renderRoutes} from 'react-router-config';
 import { HashRouter } from "react-router-dom";
-import NavBar from './components/nav-bar';
-import routes from "./router/index.js";
+import NavBar from '@/components/nav-bar';
+import routes from "@/router/index.js";
 function App() {
   return (
     <HashRouter>

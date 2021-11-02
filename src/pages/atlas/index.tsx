3028ccffcc0a691d61f9d5e5ff464fdb03d67f1a
layla <minytiny@163.com>
2021-11-02 16:51:12 +0800

    feat: added react-amap

--- src/pages/atlas/index.tsx
+++ src/pages/atlas/index.tsx
@@ -1,5 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
+import { LngLat, Map, Markers, PluginConfig, PluginList } from "react-amap";
+
+const plugins: (PluginList | PluginConfig)[] = [
+  'MapType',
+  'OverView',
+];
+const randomPosition = () => ({
+  longitude: 13.4 + Math.random() * 10,
+  latitude: 40.2 + Math.random() * 10
+})
+const randomMarker = (len: number) => (
+  Array(len).fill(true).map((e, idx) => ({
+    position: randomPosition()
+  }))
+);
+const MyApp = function () {
+
+  const [markers, setMarkers] = useState(randomMarker(50));
+  const center: LngLat = { latitude: 48.2, longitude: 16.4 };
+
+  const randomMarkers: any = () => {
+    setMarkers(randomMarker(100))
+  }
+
+  return (
+    <div>
+      <div style={{ width: '100%', height: 400 }}>
+        <Map plugins={plugins} center={center} zoom={8}>
+          <Markers
+            markers={markers}
+          />
+        </Map>
+      </div>
+      <button onClick={randomMarkers}>Random Markers</button>
+    </div>
+  )
+}
 
 export default function Atlas() {
-  return <div className="container">地图</div>;
+  return <div className="container">
+    <MyApp />
+  </div>;
 }

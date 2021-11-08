3de516a5fd6cb115293aea976ed51149c9468787
layla <minytiny@163.com>
2021-11-08 15:49:29 +0800

    feat: added echart and encapsulated weather gauge component

---
+++ src/components/gauge/index.tsx
@@ -0,0 +1,102 @@
+import React, { useEffect } from 'react';
+import * as echarts from 'echarts/core';
+import { GaugeChart } from 'echarts/charts';
+import { CanvasRenderer } from 'echarts/renderers';
+
+echarts.use([GaugeChart, CanvasRenderer]);
+
+interface Iprop {
+  score: number;
+}
+
+export default function Gauge(props: Iprop) {
+  const option = {
+    series: [
+      {
+        type: 'gauge',
+        startAngle: 180,
+        endAngle: 0,
+        min: 0,
+        max: 1,
+        splitNumber: 8,
+        axisLine: {
+          lineStyle: {
+            width: 6,
+            color: [
+              [0.25, '#FF6E76'],
+              [0.5, '#FDDD60'],
+              [0.75, '#58D9F9'],
+              [1, '#7CFFB2']
+            ]
+          }
+        },
+        pointer: {
+          icon: 'path://M12.8,0.7l12,40.1H0.7L12.8,0.7z',
+          length: '12%',
+          width: 20,
+          offsetCenter: [0, '-60%'],
+          itemStyle: {
+            color: 'auto'
+          }
+        },
+        axisTick: {
+          length: 12,
+          lineStyle: {
+            color: 'auto',
+            width: 2
+          }
+        },
+        splitLine: {
+          length: 20,
+          lineStyle: {
+            color: 'auto',
+            width: 5
+          }
+        },
+        axisLabel: {
+          color: '#464646',
+          fontSize: 20,
+          distance: -60,
+          formatter: function (value: number) {
+            if (value === 0.875) {
+              return '😍';
+            } else if (value === 0.625) {
+              return '😀';
+            } else if (value === 0.375) {
+              return '🙂';
+            } else if (value === 0.125) {
+              return '😭';
+            }
+            return '';
+          }
+        },
+        title: {
+          offsetCenter: [0, '-20%'],
+          fontSize: 30
+        },
+        detail: {
+          fontSize: 50,
+          offsetCenter: [0, '0%'],
+          valueAnimation: true,
+          formatter: function (value: number) {
+            return Math.round(value * 100) + '分';
+          },
+          color: 'auto'
+        },
+        data: [
+          {
+            value: props.score,
+            name: '今日天气🌞'
+          }
+        ]
+      }
+    ]
+  };
+  useEffect(() => {
+    let chartDom = document.getElementById('main')!;
+    let myChart = echarts.init(chartDom as HTMLDivElement);
+    myChart.setOption(option);
+  }, [option])
+
+  return <div id="main" style={{ width: 500, height: 500 }}> </div>;
+}
--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,14 @@
-import React from "react";
+import React, { useState } from "react";
+import Gauge from "@/components/gauge"
+import { Button } from "antd";
 
 export default function Home() {
+  const [score, setScore] = useState(0.8)
+  const handleClick=() => {
+    setScore(0.20)
+  }
   return (<div className="container">
-    zhuye
+    <Button onClick={handleClick}>测试</Button>
+    <Gauge score={score}/>
   </div>);
-}
+}	
